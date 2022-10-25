223e7b10b62fa7b95fe601c650ddfac2b1bef464
SHAHARIYER AHAMMED <shahariyer.as@gmail.com>
2022-10-25 21:41:14 +0600

    post and geta add

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const ObjectId = require("mongodb").ObjectId;
+
+const port = process.env.PORT || 5000;
+
+// middle ware
+app.use(cors());
+app.use(express.json());
+
+// server connect 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.wsk2b.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const parkingCollection = client
+        .db("parking_collection")
+        .collection("parking");
+
+
+        app.get("/service", async (req, res) => {
+            const query = {};
+            const cursor = parkingCollection.find(query)
+            const service = await cursor.toArray();
+            res.send(service);
+          });
+    //   post parking 
+    app.post("/parking", async (req, res) => {
+        const parking = req.body;
+        const result = await parkingCollection.insertOne(parking);
+        res.send(result);
+      });
+
+    }
+    finally {
+    }
+}run().catch(console.dir);
+ //---connect to data base ---
+// {code}
+// main async function ----------
+// {code}
+// app listening --------------
+app.get("/", (req, res) => {
+  res.send("hello Parking apps");
+});
+
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});
